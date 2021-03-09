43043c89641905477f480de2f19302788076c502
Rohith G Krishna <rohithgk2@gmail.com>
2021-03-09 12:39:26 +0530

    CommitBDD

---
+++ TempTest02/steps/step_def.py
@@ -0,0 +1,9 @@
+from behave import given,when,then
+
+@given('I am the first sample')
+def first_sample(context):
+    pass
+
+@then('I should be the first sample')
+def confirm_sample(context):
+    pass
---
+++ TempTest02/test_one.feature
@@ -0,0 +1,6 @@
+Feature: Test One
+
+    Scenario: sample scenerio
+
+        Given I am the first sample
+        Then I should be the first sample
